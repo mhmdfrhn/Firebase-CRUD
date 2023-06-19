ea23af4e4c61ffd041a0efd72d343d0e0cdc5a35
mhmdfrhn <frhn2707@gmail.com>
2023-06-19 21:34:29 +0700

    add pages

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,51 @@
+import { useContext, useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import "./login.scss";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+
+        dispatch({ type: "LOGIN", payload: user });
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Passwor"
+        />
+        <button>Login</button>
+        {error && <span>wrong email or password!</span>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
