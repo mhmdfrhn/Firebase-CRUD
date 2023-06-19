e7129035b85515516203a1a646c49a6ff9010722
mhmdfrhn <frhn2707@gmail.com>
2023-06-19 21:34:14 +0700

    add context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useReducer, useEffect } from "react";
+import AuthReducer from "./AuthReducer";
+
+//** Read current user from local storage */
+const INITIAL_STATE = {
+  currentUser: JSON.parse(localStorage.getItem("user")) || null,
+};
+
+export const AuthContext = createContext(INITIAL_STATE);
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  //** Set user to local storage */
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.currentUser));
+  }, [state.currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
